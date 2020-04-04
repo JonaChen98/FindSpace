635a4d23eb5ff2ddb02234291035b89e3f1df162
Toby <tobyau9000@gmail.com>
2020-04-03 22:08:22 -0400

    export models

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const db = require("./db");
+const Student = require("./student");
+const Professional = require("./professional");
+// const reviewStudent = require("./reviewStudent");
+
+module.exports = {
+  db,
+  Student,
+  Professional,
+}
