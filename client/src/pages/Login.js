4038b477a22efd590d33ddc466c011decd33af22
Jona <Jona.chen77@myhunter.cuny.edu>
2020-04-19 00:10:30 -0400

    fixed login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,28 @@
 import React from 'react';
-// import '../styles/Login.css';
+import '../styles/login.css';
+import personicon from '../assets/personicon.png';
+import findspacelogo from '../assets/FindspaceLogo.png';
+
+
 
 const Login = () => {
 
   return( 
-    <div>
-      <h1 className = "App">FindSpace</h1>
-      <h1 id = "slide"  >Welcome!</h1>
-      <label for="Email"><strong>School Email</strong></label><br />
-      <form className = "Loginform">
-        <input type ="text" id="Email" name ="Email" /><br />
-        <label for="pass" id="password-label"><strong>Password</strong></label><br />
-        <input type="password" id="pass" name="pass"  /> <br />
-        <input type="submit" value="Login" id="buttonsize"/>
-      </form>     
+    <div className = "background">
+      <div className = "logoandheader">
+        <img className="logo" src = {findspacelogo}></img>
+        <h1 id = "name">Findspace</h1>
+      </div>
+      <h1 id = "loginmessage">Login</h1>
+      <h1 id = "hellomessage">Hello!</h1>
+      <div className = "personicon_and_form">
+        <img className ="personicon" src = {personicon}></img>
+        <form className = "loginform">
+          <input className = "logininput" type ="text" id="Email" name ="Email"/><br/>
+          <input className = "logininput" type="password" id="pass" name="pass"/> <br />
+          <input type="submit" value="Login" id="submitbutton"/>
+        </form>  
+      </div> 
     </div>
   )
 }
