d718515caf02b0ed7901f52f2534cf5b93be70b9
Jona <Jona.chen77@myhunter.cuny.edu>
2020-04-21 00:25:00 -0400

    fixed login again

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/login.css';
-import personicon from '../assets/personicon.png';
+
 import findspacelogo from '../assets/FindspaceLogo.png';
 
 
@@ -10,13 +10,9 @@ const Login = () => {
   return( 
     <div className = "background">
       <div className = "logoandheader">
-        <img className="logo" src = {findspacelogo}></img>
-        <h1 id = "name">Findspace</h1>
       </div>
-      <h1 id = "loginmessage">Login</h1>
       <h1 id = "hellomessage">Hello!</h1>
-      <div className = "personicon_and_form">
-        <img className ="personicon" src = {personicon}></img>
+      <div>
         <form className = "loginform">
           <input className = "logininput" type ="text" id="Email" name ="Email"/><br/>
           <input className = "logininput" type="password" id="pass" name="pass"/> <br />
