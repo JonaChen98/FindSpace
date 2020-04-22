5cf4a4a219d977f8096338a4960d53b7b624d440
Jona <Jona.chen77@myhunter.cuny.edu>
2020-04-21 21:22:49 -0400

    pushing login to master

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,22 @@
 import React from 'react';
-// import '../styles/Login.css';
+import '../styles/login.css';
+
+
 
 const Login = () => {
 
   return( 
-    <div>
-      <h1 className = "App">FindSpace</h1>
-      <h1 id = "slide"  >Welcome!</h1>
-      <label for="Email"><strong>School Email</strong></label><br />
-      <form className = "Loginform">
-        <input type ="text" id="Email" name ="Email" /><br />
-        <label for="pass" id="password-label"><strong>Password</strong></label><br />
-        <input type="password" id="pass" name="pass"  /> <br />
-        <input type="submit" value="Login" id="buttonsize"/>
-      </form>     
+    <div className = "background">
+      <div className = "logoandheader">
+      </div>
+      <h1 id = "hellomessage">Hello!</h1>
+      <div>
+        <form className = "loginform">
+          <input className = "logininput" type ="text" id="Email" name ="Email"/><br/>
+          <input className = "logininput" type="password" id="pass" name="pass"/> <br />
+          <input type="submit" value="Login" id="submitbutton"/>
+        </form>  
+      </div> 
     </div>
   )
 }
