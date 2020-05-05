79ff2048f6eeebc94f37c0d639b077d6decb4c36
Jona <Jona.chen77@myhunter.cuny.edu>
2020-05-05 19:58:37 -0400

    call to api login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import '../styles/login.css';
 import {useState} from 'react';
-const router = require('express').Router();
 import logo from '../assets/FindspaceLogo.png';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import axios from 'axios';
+import {useHistory} from 'react-router-dom';
 
-function Login () {
-  const [username, setusername] = useState("");
+
+function Login(event) {
+  const [school_email, setschool_email] = useState("");
   const [password, setpassword] = useState("");
-
-
-  const handleLogin = (event) => {
+    const handleLogin = (event) => {
     event.preventDefault();
-    router.post("/api/login-student", {school_email:username, password:password})
+    axios.post('/api/login-student', {school_email:school_email, password:password})
+    
+    .then(res =>{
+      //  localStorage.setItem(school_email,school_email);
+      //  localStorage.setItem(password,password);
+      console.log(res);
+    })
+    .catch((err) => {
+
+      console.log("this is the error message:" + err);
+    })
   }
 
   return( 
@@ -26,9 +36,9 @@ function Login () {
         <h1 className = "hellomessage">Hello!</h1>
         <form className = "loginform">
           {/* event.target.value for each input */}
-          <TextField className = "logininput" label = "school_email" id = "Email" onChange = {event => setusername(event.target.value)}/><br/>
-          <TextField className = "logininput" label = "password" id = "pass" onChange = {event => setpassword(event.target.value)}/> <br />
-          <Button type="submit" id="submitbutton" onClick = {handleLogin}/>
+          <TextField className = "logininput" label = "school_email" id = "school_email" onChange = {event => setschool_email(event.target.value)} /><br/>
+          <TextField className = "logininput" label = "password" id = "password" onChange = {event => setpassword(event.target.value)} /> <br />
+          <input type = "submit" id = "submitbutton" onClick = {handleLogin}/>
         </form>  
       </div> 
     </div>
