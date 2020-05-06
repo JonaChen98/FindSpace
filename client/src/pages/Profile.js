168332944ba83d39bd81cc6b66cea74dde4095c2
Toby <tobyau9000@gmail.com>
2020-05-05 21:13:20 -0400

    update profile for rehan

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -9,39 +9,44 @@ import { Link } from 'react-router-dom';
 import Navbar from '../components/navbar';
 import Button from '@material-ui/core/Button';
 
-import UserData from '../pages/jsontest/usertest.json';
-import ProfData from '../pages/jsontest/proftest.json';
+// import UserData from '../pages/jsontest/usertest.json';
+// import ProfData from '../pages/jsontest/proftest.json';
 
 const Profile = () => {
   const [response, setRes] = useState("");
-
   
+  const [name, setName] = useState("");
+  // etc... more data points 
+
+  // componentdidMount
   useEffect(() => {
-    axios.get('/api')
-      .then(res => {
-        console.log(res.data);
-        setRes(res.data);
-      })
-      .catch(err => {
-        console.log(err);
-      })
+    // localstorage get data 
+    // set your data to state 
+    // setName(name) -> name 
+    
+    // axios.get('/api')
+    //   .then(res => {
+    //     console.log(res.data);
+    //     setRes(res.data);
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   })
   }, []);
 
 
-  useEffect(() => {
+  // useEffect(() => {
 
-    const fetchData = async () => {
+  //   const fetchData = async () => {
 
-      const res = await axios.get('/api/students');
-      setRes(res.data);
-      const data = await response.json();
-      this.setState({ person: data.results[0] });
-    }
-    fetchData();
-  }, []); 
-
-  var x = 1;
-  if(x === 1){
+  //     const res = await axios.get('/api/students');
+  //     setRes(res.data);
+  //     const data = await response.json();
+  //     this.setState({ person: data.results[0] });
+  //   }
+  //   fetchData();
+  // }, []); 
+  
   return (
     <div className="profile-page">
       <div>
@@ -53,7 +58,7 @@ const Profile = () => {
               <img className="profile-image" src="https://www.petlandflorida.com/wp-content/uploads/2019/09/Petland_Florida_Cavalier_King_Charles_Spaniel_puppy.jpg" alt="did not load"></img>
             </div>
             <div className="name">
-              {this.state.person.name}
+              {name}
             </div>
             <div className="major">
               {UserData.map((UserJson, index)=>{
@@ -94,61 +99,9 @@ const Profile = () => {
           </div>
           </div>
        </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
-  }
-  else if(x === 0){
-    return (
-      <div className="profile-page">
-        <div>
-        <Navbar />
-          <div className="profile-container">
-            <h1> Profession Profile</h1>
-            <div className="userInfo-container">
-              <div>
-                <img className="profile-image" src="https://www.petlandflorida.com/wp-content/uploads/2019/09/Petland_Florida_Cavalier_King_Charles_Spaniel_puppy.jpg" alt="did not load"></img>
-              </div>
-              <div className="name">
-                {UserData.map((UserJson, index)=>{
-                  return <p><b>{UserJson.name}</b></p>
-                  })}
-              </div>
-              <div className="major">
-                {UserData.map((UserJson, index)=>{
-                  return <p>{UserJson.major} Major</p>
-                  })}
-              </div>
-              <div className="school">
-                {UserData.map((UserJson, index)=>{
-                  return <p>School: {UserJson.school}</p>
-                  })}
-              </div>
-              <div className="email">
-                {UserData.map((UserJson, index)=>{
-                  return <p>Email: {UserJson.email} </p>
-                  })}
-              </div>
-              <div className="phone">
-                {UserData.map((UserJson, index)=>{
-                  return <p>Phone: {UserJson.phone}</p>
-                  })}
-              </div>
-            </div>
-            <div className="workspacedesc1">
-              {
-                ProfData.map((ProfJson, index)=>{
-                return<p><b>workspace description:</b><br></br>
-                {ProfJson.workspace1}</p>
-                })
-              }
-              </div>
-            </div>
-         </div>
-        <Footer />
-      </div>
-    );
-    }
 }
 
 
