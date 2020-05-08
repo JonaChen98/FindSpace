c6d9bc28d119eb31b7e63479f1c5b2ea74aab4c5
R-Ligier <rebeccal784@gmail.com>
2020-05-07 23:04:28 -0400

    redesigned signup page, not finished. need to clean code up

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,14 +3,13 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
-import { CardContent } from '@material-ui/core';
-import CardActions from '@material-ui/core/CardActions';
-import CardMedia from '@material-ui/core/CardMedia';
+
 import Typography from '@material-ui/core/Typography';
 import Professional from '../assets/professional.png';
 import Student from '../assets/student.png';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import '../styles/signup.css';
 
 const useStyles = makeStyles({
   // root: {
@@ -29,90 +28,50 @@ export default function ContainedButtons() {
   <div>
     <Navbar/>
 
-    <h1 style={{textAlign:"center"}}> Tell us who you are.</h1>
     
-    <div style={{marginTop: "40%",margin: "auto", width: "50%", padding: "10px", display: "flex"}}>
+    <Typography variant="h4" style={{textAlign:"center", paddingTop:"50px"}}> Tell us who you are.</Typography>
 
-      <div id = "studentCard" style={{ width: "40%", float: "left", padding: "20px"}}>
-        <Card>
-          <CardContent>
-            <Typography 
-              gutterBottom variant="h5" 
-              component="h2"  
-              style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-              }}
-              >
-                STUDENT
-            </Typography>
-          </CardContent>
-          
-          <CardMedia className={classes.media} image={Student}/>
-          
-          <CardContent>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Join as a student and browse through spaces offered by companies and individuals.
-            </Typography>
-          </CardContent>
+    <div className="container">
+        <div className="student-container">
+          {/* <Typography variant="h5" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <b>Student</b>
+          </Typography> */}
+        
+          <img src={Student} alt="student" className="img"/>
 
-          <CardActions style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-              }}>
-            <Button variant="contained" color="primary" component={Link} to="/ProfileSetup_Student">
-            Join as a student
+            <Typography variant="h6" className="description">
+                Join as a student to browse through spaces offered by companies and individuals.
+            </Typography>
+
+            <div className="button">
+              <Button variant="outlined" size="large" color="primary" className="button" component={Link} to="/ProfileSetup_Student">
+              <b> Join as a student </b>
+              </Button>
+            </div>
+          </div>
+            
+        <div className="prof-container">
+          {/* <Typography variant="h5" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <b>Professional</b>
+          </Typography> */}
+            
+          <img src={Professional} alt="student" className="img"/>
+          
+          <Typography variant="h6" className="description">
+            Join as a professional to offer students a space for quiet study and school work.
+          </Typography>
+          
+          <div className="button">
+            <Button variant="outlined" size="large" color="primary" component={Link} to="/ProfileSetup_Prof">
+              <b>Join as a professional</b>
             </Button>
-          </CardActions>
-
-        </Card>
-
+          </div>
+     
         </div>
-
-        <div id = "professionalCard" style={{ width: "40%", float: "left", padding: "20px"}}>
-        <Card>
-        {/* <Card className={classes.root}> */}
-          <CardContent>
-            <Typography 
-              gutterBottom variant="h5" 
-              component="h2"  
-              style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-              }}
-              >
-                PROFESSIONAL
-            </Typography>
-          </CardContent>
-          
-            <CardMedia
-              className={classes.media}
-              image={Professional}
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">
-              Join as a professional to offer students a space for studying and school work.
-              </Typography>
-            </CardContent>
-            <CardActions style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center"
-              }}>
-            {/* TODO: Change link  */}
-            <Button variant="contained" color="primary" component={Link} to="/ProfileSetup_Prof">
-            Join as a professional
-            </Button>
-          </CardActions>
-        </Card>
-      </div>
-      <hr>
-      </hr>
+  
+      
     </div>
+    <hr/>
     <Footer/>
   </div>
   
