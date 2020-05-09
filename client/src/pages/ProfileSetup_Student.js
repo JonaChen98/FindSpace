0ec7ca39f27ab7675108cfe4e81dba04c1dc4fe1
R-Ligier <rebeccal784@gmail.com>
2020-05-09 13:33:57 -0400

    Added form validation to profile setup page for students, need to finish design

--- client/src/pages/ProfileSetup_Student.js
+++ client/src/pages/ProfileSetup_Student.js
@@ -1,91 +1,283 @@
 import React, {useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Navbar from '../components/navbar';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Footer from '../components/footer';
 import axios from 'axios';
+import '../styles/profilesetup.css';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-      width: '60ch',
-    },
-  },
-})); 
 
-function ProfileSetupStudent() {
-  const classes = useStyles();
-  
-  const history = useHistory();
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [major, setMajor] = useState("");
-  const [age, setAge] = useState(0);
 
-  const signUp = (e) => {
-    e.preventDefault();
+const initialState = {
+  name: "",
+  email: "",
+  age: "",
+  password: "",
+  major: "",
+  nameError: "",
+  emailError: "",
+  passwordError: ""
+};
+
+export default class ValiationForm extends React.Component {
+  state = initialState;
+
+  handleChange = event => {
+    const isCheckbox = event.target.type === "checkbox";
+    this.setState({
+      [event.target.name]: isCheckbox
+        ? event.target.checked
+        : event.target.value
+    });
+  };
+
+  // onChange = (e) => {
+  //   /*
+  //     Because we named the inputs to match their
+  //     corresponding values in state, it's
+  //     super easy to update the state
+  //   */
+  //   this.setState({ [e.target.name]: e.target.value });
+  // }
+
+  // signUp = (e) => {
+  //   e.preventDefault();
+
     
-    axios.post('/api/register-student', {
-        "name": name, 
-        "password": password, 
-        "age": age, 
-        "school_email": email, 
-        "major": major
-      })
-      .then(res => {
-        localStorage.setItem("studentInfo", JSON.stringify(res.data.student));
-        history.push('/student-dashboard');
-      })
-      .catch((err) => {
-        console.log("Error");
-      })
-  }
-  return (
+
+    // const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+    // const [major, setMajor] = useState("");
+    // const [age, setAge] = useState(0);
+    
+  //   axios.post('/api/register-student', {
+  //       name: this.state.name, 
+  //       password: this.state.password, 
+  //       age: this.state.age, 
+  //       school_email: this.state.email, 
+  //       major: this.state.major
+  //     })
+  //     .then(res => {
+  //       localStorage.setItem("studentInfo", JSON.stringify(res.data.student));
+  //      // history.push('/student-dashboard');
+  //     })
+  //     .catch((err) => {
+  //       console.log("Error");
+  //     })
+  // }
+
+  validate = () => {
+    let nameError = "";
+    let emailError = "";
+    let passwordError = "";
+    let ageError = "";
+    let majorError = "";
+
+    if (!this.state.name) {
+      nameError = "Please enter your first and last name.";
+    }
+
+    if (!this.state.email.includes(".edu")){
+      emailError = "invalid email";
+    }
+    if (isNaN(this.state.age)){
+      ageError = "Enter a number.";
+    }
+    
+    if (this.state.password.length < 8){
+      passwordError = "Password must be 8 characters";
+    }
+
+    if (emailError || nameError || passwordError|| ageError || majorError) {
+      this.setState({ emailError, nameError, passwordError, 
+        ageError, majorError });
+      return false;
+    }
+
+    return true;
+  };
+
+  
+  handleSubmit = event => {
+    event.preventDefault();
+    const isValid = this.validate();
+    const history = useHistory();
+      
+    if (isValid) {
+      console.log(this.state);
+      // clear form
+      this.setState(initialState);
+      // console.log(this.state.name);
+      axios.post('/api/register-student', {
+              name: this.state.name, 
+              password: this.state.password, 
+              age: this.state.age, 
+              school_email: this.state.email, 
+              major: this.state.major
+            })
+            .then(res => {
+              localStorage.setItem("studentInfo", JSON.stringify(res.data.student));
+             history.push('/student-dashboard');
+            })
+            .catch((err) => {
+              console.log("Error");
+            })
+      
+    }
+  };
+
+  render() {
+    return (
     <div>
       <Navbar/>
-      <div>
-        <h1 style={{textAlign:"center"}}> Tell us about yourself.</h1>
-        <form className={classes.root} noValidate autoComplete="off" style={{margin: "auto", width: "50%", padding: "10px"}}>
-            <h4> Full Name: </h4>
-            <TextField id="margin-none-standard-multiline-flexible" onChange={event => setName(event.target.value)}/>
-            <h4> Age: </h4>
-            <TextField id="margin-none" onChange={event => setAge(event.target.value)}/>
-            <h4> School Email: </h4>
-            <TextField id="margin-none" onChange={event => setEmail(event.target.value)}/>
-            <h4> Password: </h4>
-            <TextField id="margin-none" onChange={event => setPassword(event.target.value)}/>
-            <h4> School Name: </h4>
-            <TextField id="margin-none"/>
-            <h4> Major: </h4>
-            <TextField id="margin-none" onChange={event => setMajor(event.target.value)}/>
-        </form>
-      </div>
-    
-    <div>
-      <form className={classes.root} noValidate autoComplete="off" style={{margin: "auto", width: "50%", padding: "10px"}}>
-        <h4> Briefly describe yourself</h4>
-          <TextField id="standard-basic" variant="outlined" />
-        <h4> What do you intend to use the desk space if given?</h4>
-          <TextField id="standard-basic" variant="outlined" />
+        <div  className="center">
+          <form onSubmit={this.handleSubmit} className="form">
+            <div>
+            <label> Full Name: </label>
+              <TextField
+                name="name"
+                placeholder="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+              <div style={{ fontSize: 12, color: "red" }}>
+                {this.state.nameError}
+              </div>
+            </div>
 
-        <Button variant="contained" type="submit" color="primary" onClick={(e) => signUp(e)} style={{justifyContent: "center", alignItems: "center"}}>
-            Submit
-        </Button>
-      </form>
-       
-      <hr>
-      </hr>
+            <div className="text-field">
+              <h4> Age: </h4>
+              <TextField 
+                label="Required*"
+                name="age"
+                placeholder="age"
+                value={this.state.age}
+                onChange={this.handleChange}
+                
+              />
+              
+              {/* <div style={{ fontSize: 12, color: "red" }}> */}
+                {this.state.ageError}
+              {/* </div> */}
+            </div>
+
+            <div>
+            <label> Major: </label>
+            <TextField
+                name="major"
+                placeholder="major"
+                value={this.state.major}
+                onChange={this.handleChange}
+              />
+              <div style={{ fontSize: 12, color: "red" }}>
+                {this.state.majorError}
+              </div>
+            </div>
+            <div>
+            <TextField
+                name="email"
+                fullWidth
+                placeholder="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+              <div style={{ fontSize: 12, color: "red" }}>
+                {this.state.emailError}
+              </div>
+            </div>
+            <div>
+            <TextField
+                type="password"
+                name="password"
+                placeholder="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+              <div style={{ fontSize: 12, color: "red" }}>
+                {this.state.passwordError}
+              </div>
+            </div>
+            <div>
+              <h4> Briefly describe yourself</h4>
+                  <TextField id="standard-basic" variant="outlined" />
+              <h4> What do you intend to use the desk space if given?</h4>
+                  <TextField id="standard-basic" variant="outlined" />
+            </div>
+            <Button variant="contained" type="submit" color="primary" style={{justifyContent: "center", alignItems: "center"}}>
+                Submit
+            </Button>
+          </form>
+        </div>
+      
+        
+      {/* <Footer/> */}
+      
     </div>
-    
-    <Footer/>
-    
-    </div>
-  );
+      
+    );
+  }
 }
 
-export default ProfileSetupStudent;
+// function ProfileSetupStudent() {
+//   // const classes = useStyles();
+  
+//   const history = useHistory();
+
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [password, setPassword] = useState("");
+//   const [major, setMajor] = useState("");
+//   const [age, setAge] = useState(0);
+
+
+
+//   return (
+//     <div>
+//       <Navbar/>
+      
+//       <div className="center">
+
+//       <div>
+//         <h1 style={{textAlign:"center"}}> Tell us about yourself.</h1>
+//         <form onSubmit=noValidate autoComplete="off">
+  
+//             <h4> Full Name: </h4>
+//             <TextField required id="standard-required" name="checkname" label="required" onChange={event => setName(event.target.value)}/>
+//             <h4> Age: </h4>
+//             <TextField required id="margin-none" onChange={event => setAge(event.target.value)}/>
+//             <h4> School Email: </h4>
+//             <TextField required id="margin-none" onChange={event => setEmail(event.target.value)}/>
+//             <h4> Password: </h4>
+//             <TextField id="margin-none" onChange={event => setPassword(event.target.value)}/>
+//             <h4> School Name: </h4>
+//             <TextField id="margin-none"/>
+//             <h4> Major: </h4>
+//             <TextField id="margin-none" onChange={event => setMajor(event.target.value)}/>
+//         </form>
+//       </div>
+    
+//     <div>
+//       <form noValidate autoComplete="off" style={{margin: "auto", width: "50%", padding: "10px"}}>
+//         <h4> Briefly describe yourself</h4>
+//           <TextField id="standard-basic" variant="outlined" />
+//         <h4> What do you intend to use the desk space if given?</h4>
+//           <TextField id="standard-basic" variant="outlined" />
+
+//         <Button variant="contained" type="submit" color="primary" onClick={(e) => signUp(e)} style={{justifyContent: "center", alignItems: "center"}}>
+//             Submit
+//         </Button>
+//       </form>
+//     </div>
+//     </div>
+//     <hr/>
+//     <Footer/>
+    
+//     </div>
+//   );
+          
+// };
+
+// export default ProfileSetupStudent;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 
