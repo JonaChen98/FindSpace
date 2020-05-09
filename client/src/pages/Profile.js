6e4313a0d9e11926d2ebdb4339d068ba014cdf50
Rehan Khan <rehanworkemail@gmail.com>
2020-05-08 19:15:45 -0700

    Made sure local storage display works

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,7 +8,7 @@ import { Link } from 'react-router-dom';
 
 import Navbar from '../components/navbar';
 import Button from '@material-ui/core/Button';
-import { TableBody } from '@material-ui/core';
+import { TableBody, Container } from '@material-ui/core';
 
 
 const Profile = () => {
@@ -19,46 +19,30 @@ const Profile = () => {
   const [email, setEmail] = useState("");
   const [major, setMajor] = useState("");
   // etc... more data points 
-
-  /*const [rows, setRows] = useState([
-    {names:"", school1:"", major1:"", school_email1: ""},
-    {
-      name1:"dan",
-      school1: "School ",
-      major1: "comp sci",
-      school_email1: "tb@a.com"
-    }
-  ]);*/
+  
   // componentdidMount
+
+  //test ProfileInfo display
+  //const myobj = {"name":"John", "school":"no", "school_email":"a@a.com", "major":"comp sci"};
+
   useEffect(() => {
     
       let studentInfo = localStorage.getItem("studentInfo");
       studentInfo = JSON.parse(studentInfo);
       
-      if(studentInfo){
+      //if(studentInfo){
       setName(studentInfo.name);
       setSchool(studentInfo.school);
       setEmail(studentInfo.school_email);
       setMajor(studentInfo.major);
-  }
+  //}
     }, []);
 
-    /*useEffect(() => {
-      localStorage.setItem("studentInfos", JSON.stringify(rows));
-    });
-
+    //Test ProfileInfo Display
+    /* 
     useEffect(() => {
-    
-      let studentInfo = localStorage.getItem("studentInfos");
-      studentInfo = JSON.parse(studentInfos);
-      
-      if(studentInfo){
-      setName(studentInfo.name1);
-      setSchool(studentInfo.school1);
-      setEmail(studentInfo.school_email1);
-      setMajor(studentInfo.major1);
-  }
-    }, []);*/
+      localStorage.setItem("studentInfo", JSON.stringify(myobj));
+    });*/
 
  
   return(
@@ -72,6 +56,7 @@ const Profile = () => {
       </div>
               
       <div className="userInfo-container">
+        <Container />
         <div className="name">
             {name}
         </div>
@@ -84,6 +69,7 @@ const Profile = () => {
         <div className="school">
           {school}
         </div>
+        <Container />
       </div>
               
       <div>
